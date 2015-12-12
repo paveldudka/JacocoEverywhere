8a3eab90d89baebc3b8f4a566246c9c3b19f7d2c
Pavel Dudka <dudka.pavel@gmail.com>
2015-12-12 14:33:37 -0800

    Add plugin META-INF + added plugin dependency to demo project

--- plugin/src/main/groovy/com/trickyandroid/jacocoeverywhere/JacocoEverywhere.groovy
+++ plugin/src/main/groovy/com/trickyandroid/jacocoeverywhere/JacocoEverywhere.groovy
@@ -2,12 +2,61 @@ package com.trickyandroid.jacocoeverywhere
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 
 class JacocoEverywhere implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        def task = project.task("test") << {
-            println "Hello from test!   "
+        createTasks(project)
+    }
+
+    void createTasks(Project project) {
+        createDeviceCheckerTask(project)
+        createJacocoFixerTask(project)
+
+        project.afterEvaluate {
+            fixJacoco.dependsOn(connectedDebugAndroidTest)
+            testDebugUnitTest.mustRunAfter fixJacoco
+            createDebugAndroidTestCoverageReport.dependsOn fixJacoco
+        }
+
+        project.afterEvaluate {
+            def append = "append=true"
+            def destFile = "destfile=$buildDir/outputs/code-coverage/connected/coverage.ec"
+            testDebugUnitTest.jvmArgs "-javaagent:$buildDir/intermediates/jacoco/jacocoagent.jar=$append,$destFile"
+
+            createDebugAndroidTestCoverageReport.dependsOn testDebugUnitTest
+            testDebugUnitTest.dependsOn unzipJacocoAgent
+        }
+    }
+
+    /**
+     * reroute Jacoco coverage executable to be stored on sdcard instead of built-in
+     * /data/data/<deviceid>/ location since this location is not accessible on some Samsung devices
+     * @param project
+     */
+    void createJacocoFixerTask(Project project) {
+        def task = project.task("fixJacoco", type: Exec) {
+            dependsOn getFirstConnectedDevice
+            commandLine(
+                    "${rootProject.getSdkLocation()}/platform-tools/adb",
+                    '-s',
+                    "${-> getFirstConnectedDevice.device()}",
+                    'pull',
+                    '/sdcard/coverage.ec',
+                    "$project.buildDir/outputs/code-coverage/connected/coverage.ec")
+        }
+    }
+
+    void createDeviceCheckerTask(Project project) {
+        def task = project.task("getFirstConnectedDevice", type: Exec) {
+            commandLine("${rootProject.getSdkLocation()}/platform-tools/adb", 'devices')
+            standardOutput = new ByteArrayOutputStream()
+            ext.device = {
+                if (standardOutput.toString()) {
+                    return standardOutput.toString().split('\n')[1].split("\\W+")[0]
+                }
+            }
         }
     }
 }
