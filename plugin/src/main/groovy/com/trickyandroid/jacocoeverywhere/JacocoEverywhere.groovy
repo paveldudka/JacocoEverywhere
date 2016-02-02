5b9eff0ba68a767fd209b213f051de34a96c41d6
Pavel Dudka <dudka.pavel@gmail.com>
2016-02-01 23:18:54 -0800

    Remove test code

--- plugin/src/main/groovy/com/trickyandroid/jacocoeverywhere/JacocoEverywhere.groovy
+++ plugin/src/main/groovy/com/trickyandroid/jacocoeverywhere/JacocoEverywhere.groovy
@@ -5,19 +5,10 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.logging.Logger
-import org.slf4j.Marker
-import org.slf4j.MarkerFactory
 
 class JacocoEverywhere implements Plugin<Project> {
     private Logger logger
-    private Marker marker
     final static String LOG_TAG = "[Jacoco-Everywhere]"
-
-    //coverage report task supplied by Android plugin
-    String COVERAGE_REPORT_TASK_NAME = "createDebugAndroidTestCoverageReport"
-    //unit test task provided by Android plugin
-    String UNIT_TEST_TASK_NAME = "testDebugUnitTest"
-    String INTEGRATION_TEST_TASK_NAME = "connectedDebugAndroidTest"
     String JACOCO_AGENT_UNZIPPER_TASK_NAME = "unzipJacocoAgent"
 
     @Override
@@ -29,47 +20,12 @@ class JacocoEverywhere implements Plugin<Project> {
         }
 
         this.logger = project.getLogger()
-        this.marker = MarkerFactory.getMarker(LOG_TAG)
 
         project.afterEvaluate {
             project.android.applicationVariants.all {
                 configureVariant(project, it)
             }
         }
-
-//        project.ext.coverageEnabled = { project.android.buildTypes.debug.testCoverageEnabled }
-//
-//        project.gradle.taskGraph.whenReady {
-//            println "taskGraph ready"
-//            def coverageReportTask = project.gradle.taskGraph.getAllTasks().find {
-//                it.name == COVERAGE_REPORT_TASK_NAME
-//            }
-//            def unitTestTask = project.gradle.taskGraph.getAllTasks().find { it.name == UNIT_TEST_TASK_NAME }
-//
-//            //invalidate existing unit test results if we gather coverage.
-//            if (coverageReportTask != null && unitTestTask != null) {
-//                unitTestTask.outputs.upToDateWhen { false }
-//            }
-//        }
-//
-//        project.afterEvaluate {
-//            if (project.coverageEnabled()) {
-//                logD("Coverage enabled. Applying modifications")
-//                project.tasks.matching { it.name == UNIT_TEST_TASK_NAME }.all {
-//                    def append = "append=true"
-//                    def destFile = "destfile=${project.buildDir}/outputs/code-coverage/connected/coverage.ec"
-//                    it.jvmArgs "-javaagent:${project.buildDir}/intermediates/jacoco/jacocoagent.jar=$append,$destFile"
-//                    it.dependsOn JACOCO_AGENT_UNZIPPER_TASK_NAME
-//                    it.mustRunAfter INTEGRATION_TEST_TASK_NAME
-//                }
-//
-//                project.tasks.matching { it.name == COVERAGE_REPORT_TASK_NAME }.all {
-//                    it.dependsOn UNIT_TEST_TASK_NAME
-//                }
-//            } else {
-//                project.logger.debug("Coverage is disabled. Skipping modifications")
-//            }
-//        }
     }
 
     void configureVariant(Project project, def variant) {
@@ -105,7 +61,7 @@ class JacocoEverywhere implements Plugin<Project> {
     }
 
 
-    def getTask(Project project, String name) {
+    static def getTask(Project project, String name) {
         project.getTasksByName(name, false)?.getAt(0)
     }
 
